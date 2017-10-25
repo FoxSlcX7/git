d6371d0906c1133d523833bb12a8bc99828f0e99
Ivanplat <Platonov20030603@gmail.com>
2017-10-25 15:03:08 +0300

    1
    1

---
+++ 123.php
@@ -0,0 +1,14 @@
+<?php
+
+?><!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "UTF-8">
+		<link href="main.css">
+	</head>
+			<body>
+			<div id="header">
+				<h1 class = "int">INT</h1>
+			</div>
+		</body>
+</html>
